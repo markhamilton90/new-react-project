0a062c220c8fcedaba39685749ac56471e6b1ea9
markhamilton90 <msh387@gmail.com>
2021-08-23 22:12:15 -0500

    adding some styles for later

--- src/components/Track.js
+++ src/components/Track.js
@@ -2,15 +2,23 @@ import React from 'react';
 
 class Track extends React.Component {
 
-    dragStartTrack = (e, index) => {
+    dragStart = (e, index) => {
         e.dataTransfer.setData("text", index);
     }
 
-    dragOverTrack = (e, index) => {
+    dragOver = (e, index) => {
         e.preventDefault();
+        // e.currentTarget.classList.add('highlighted');
     }
 
-    dropTrack = (e, targetIndex) => {
+    // dragEndTrack = (e) => {
+    //     if (e.currentTarget.classList.contains('highlighted')) {
+    //         e.currentTarget.classList.remove('highlighted');
+    //         console.log(e.currentTarget);
+    //     }
+    // }
+
+    drop = (e, targetIndex) => {
         const draggedIndex = e.dataTransfer.getData("text");
         this.props.reorderTracks(targetIndex, draggedIndex)
         e.dataTransfer.clearData();
@@ -18,7 +26,7 @@ class Track extends React.Component {
 
     render() {
         return (
-            <div className="track" draggable="true" onDragStart={ e => this.dragStartTrack(e, this.props.index) } onDragOver={ e => this.dragOverTrack(e, this.props.index) } onDrop={ e => this.dropTrack(e, this.props.index) }>
+            <div className="track" draggable="true" onDragStart={ e => this.dragStart(e, this.props.index) } onDragOver={ e => this.dragOver(e, this.props.index) } onDrop={ e => this.drop(e, this.props.index) }>
                 <h3>{this.props.details.name}</h3>
                 <h4>{this.props.details.artist}</h4>
                 <div className="remove-track" onClick={ () => this.props.removeTrack(this.props.index) }></div>
