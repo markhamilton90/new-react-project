b24b14d11e651a51153ab90f257b4550cc532317
markhamilton90 <msh387@gmail.com>
2022-04-17 21:09:24 -0500

    test

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import { render } from 'react-dom';
 import Router from './components/Router';
 import './css/style.css';
 
+console.log('test');
+
 render(<Router />, document.getElementById('main'));
